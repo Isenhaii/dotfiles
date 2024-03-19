98bf2fcf3e50211b24d3cf34ea1fe08bd84bc81a
Allan Wheelock <wheelock.4@wright.edu>
2024-03-18 22:04:22 -0700

    Updated script

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Chase did this one. And I copied the examples in class
 
 if [ "$(id -u)" -eq 0 ]; then
-	echo "Script is running as root"
+    echo "Script is running as root"
+
+    echo $(which apt)
+    if [[ -n "$(which apt)" ]]; then
+        echo "apt is installed exactly as specified."
+    else
+        echo "apt is not installed at the specified location."
+    fi
+
 else
-	echo "Script is not running as root"
+    echo "Script is not running as root"
 fi
+
