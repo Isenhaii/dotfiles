e36f8d8c56e366658e93a369531ac61bf060a40f
Allan Wheelock <wheelock.4@wright.edu>
2024-03-20 09:11:37 -0700

    upadte

--- install.sh
+++ install.sh
@@ -11,17 +11,17 @@ if [ "$(id -u)" -eq 0 ]; then
 
 
 # Define Anaconda version and installation directory
-ANACONDA_VERSION="2024.02.-1" # Specify the version you want to install
-INSTALL_DIR="$HOME/anaconda3"  # Specify the installation directory
+	ANACONDA_VERSION="2024.02.-1" # Specify the version you want to install
+	INSTALL_DIR="$HOME/anaconda3"  # Specify the installation directory
 
 # Download Anaconda installer script
-curl -O https://repo.anaconda.com/archive/Anaconda3-<2024.02.-1>-Linux-x86_64.sh
+	curl -O https://repo.anaconda.com/archive/Anaconda3-<2024.02.-1>-Linux-x86_64.sh
 
 # Run Anaconda installer script
-bash anaconda_installer.sh -b -p $INSTALL_DIR
+	bash anaconda_installer.sh -b -p $INSTALL_DIR
 
 # Remove Anaconda installer script
-rm anaconda_installer.sh
+	rm anaconda_installer.sh
 
     else
         echo "apt is not installed at the specified location."
