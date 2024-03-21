37185870885ed5488af10e4e54c6c3df0bbb9ea5
Allan Wheelock <wheelock.4@wright.edu>
2024-03-21 16:20:49 -0700

    2

--- install.sh
+++ install.sh
@@ -4,12 +4,26 @@
 if [ "$(id -u)" -eq 0 ]; then
     echo "Running as root!"
 else
-    echo "Not running as root!"
+    echo "NOT running as root!"
 fi
+
+sleep 3
 
 #Check if the apt package manager is installed
 if which apt >/dev/null 2>&1; then
     echo "apt package manager is installed"
+
+sleep 3
+
+	echo "Installing dnsutils..."
+
+sleep 3
+
+	#If apt is installed, do the following
+	sudo apt update
+	sudo apt install dnsutils
+    echo "dnsutils has been installed."
+
 else
     echo "apt package manager is NOT installed"
 fi
