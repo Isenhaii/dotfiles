f1c682154ba643397b928470d75136fbb3a75798
Allan Wheelock <wheelock.4@wright.edu>
2024-03-21 16:04:33 -0700

    1

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ if [ "$(id -u)" -eq 0 ]; then
 else
     echo "Not running as root!"
 fi
+
+#Check if the apt package manager is installed
+if which apt >/dev/null 2>&1; then
+    echo "apt package manager is installed"
+else
+    echo "apt package manager is NOT installed"
+fi
