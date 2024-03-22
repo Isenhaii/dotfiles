ab67c2bdb1aebc4bb42bd160741f5e10d508be41
Allan Wheelock <wheelock.4@wright.edu>
2024-03-21 17:16:17 -0700

    5

--- install.sh
+++ install.sh
@@ -7,23 +7,65 @@ else
     echo "NOT running as root!"
 fi
 
-sleep 3
-
+sleep 2
 #Check if the apt package manager is installed
 if which apt >/dev/null 2>&1; then
-    echo "apt package manager is installed"
+    echo "apt package manager is installed, proceeding..."
 
-sleep 3
 
+
+sleep 2
 	echo "Installing dnsutils..."
-
-sleep 3
-
-	#If apt is installed, do the following
+sleep 2
+	if which dig >/dev/null 2>&1; then
+    echo "dnsutils is already installed!"
+	else
+sleep 2
+	#If apt is installed and nmap is NOT installed, do the following
 	sudo apt update
 	sudo apt install dnsutils
     echo "dnsutils has been installed"
+	fi
+
+
+
+
+sleep 2
+	echo "Installing nmap..."
+sleep 2
+	if which nmap >/dev/null 2>&1; then
+    echo "nmap is already installed!"
+	else
+sleep 2
+	#If apt is installed and nmap is NOT installed, do the following
+	sudo apt install nmap
+    echo "nmap has been installed"
+	fi
+
+
+
+
+sleep 2
+	echo "Installing cmatrix..."
+sleep 2
+	if which cmatrix >/dev/null 2>&1; then
+    echo "cmatrix is already installed!"
+	else
+sleep 2
+	#If apt is installed and cmatrix is NOT installed, do the following
+	sudo apt install cmatrix
+    echo "cmatrix has been installed"
+	fi
+
+
+
+
+
+
+
+
+
 
 else
-    echo "apt package manager is NOT installed"
+    echo "Error: apt package manager is NOT installed"
 fi
